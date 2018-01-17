f27f79862d1c6ebb98fb8b02a1edfd64b6e8156d
Pseunition <fawinheightz@gmail.com>
2018-01-17 23:17:21 +0200

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,16 @@
+#include "toad.h"
+#include "stdafx.h"
+#include "yrmfs.h"
+#include <iostream>
+
+using harp-toad-api;
+
+int main()
+{
+  toad.fetch(dbharp.lash%dbharp.pieceIndex);
+  std::als.get(toad);
+  return toad.fulldb;
+
+
+  return 0;
+}
